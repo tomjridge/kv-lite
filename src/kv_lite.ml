9150526370d06f89ad334be7c28564674f523832
Tom Ridge <tom.j.ridge@googlemail.com>
2021-09-06 11:22:42 +0100

    +

--- src/kv_lite.ml
+++ src/kv_lite.ml
@@ -3,7 +3,8 @@ module type S_LWT = Impl_intf.S_LWT
 include (Impl : S_LWT)
 
 (** This is a direct-style interface, assuming Lwt main is running in
-   another system thread *)
+   another system thread. NOTE the ocamldoc is not very good - type
+   ['a M.t] here is just ['a] *)
 module Direct = Direct.Make_2
 
 module Private = struct
